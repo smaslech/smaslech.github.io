20c0e34679e8c6bee27fd301d887cb5da8980649
smaslech <s2m42@outlook.com>
2018-10-31 02:33:03 -0400

    tests

--- private/js/dynamic.js
+++ private/js/dynamic.js
@@ -1,23 +1,85 @@
-$(function () {
+window.onload = function() {
+  var pages = {
+    index: {
+      title: "Test",
+      url: "test.html",
+      content: ""
+    },
+    about: {
+      title: "Info",
+      url: "info.html",
+      content: ""
+    }
+  }
 
-    $("#info").click(function (e) {
-        $el = $(this);
-        e.preventDefault();
-        $('body').addClass("info-open");
-        history.pushState({
-            type: 'info'
-        }, "Sarah Maslechko | " + $el.data('title'), $el.attr('href'));
+  // Get references to the page elements.
+  var navLinks = document.querySelectorAll('.btn');
+  var contentElement = document.getElementById('info-modal');
+
+
+  // Update the page content.
+  var updateContent = function(stateObj) {
+    // Check to make sure that this state object is not null.
+    if (stateObj) {
+      contentElement.innerHTML = stateObj.content;
+    }
+  };
+
+
+  // Load the page content via AJAX.
+  var loadContent = function(url, callback) {
+    var request = new XMLHttpRequest();
+
+    request.onload = function(response) {
+      // Save the html in the pages object so that it doesn't need
+      // to be loaded again.
+      pages[url.split('.')[0]].content = response.target.response;
+
+      var pageData = pages[url.split('.')[0]];
+
+      // Update the title and content.
+      updateContent(pageData);
+      
+      // Execute the callback function.
+      callback();
+    };
+
+    request.open('get', 'pages/' + url, true);
+    request.send();
+  };
+
+
+  // Attach click listeners for each of the nav links.
+  for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', function(e) {
+      e.preventDefault();
+
+      // Fetch the page data using the URL in the link.
+      var pageURL = this.attributes['href'].value;
+
+      loadContent(pageURL, function() {
+        var pageData = pages[pageURL.split('.')[0]];
+
+        // Create a new history item.
+        history.pushState(pageData, pageData.title, pageURL);
+      });
     });
+  }
+  
 
-    window.addEventListener('popstate', function(e){
-      if(e.state)
-        openURL(e.state.href);
-   }); 
+  // Update the page content when the popstate event is called.
+  window.addEventListener('popstate', function(event) {
+    updateContent(event.state);
+  });
 
-    $(window).click(function(event) {
-    	var target = $( event.target );
-    	if ( target.is( "#closeModal" ) ) {
-    		$('body').removeClass('info-open')
-    	}
-    });
-});
+
+  // Load initial content.
+  loadContent('test.html', function() {
+    // Update this history event so that the state object contains the data
+    // for the homepage.
+    history.replaceState(pages.index, pages.index.title, '');
+  });
+  
+};
+
+

9d6cc719cac35b448c8281dd150f2199e7311548
smaslech <s2m42@outlook.com>
2018-11-04 15:49:55 -0500

    pushstate testing

--- private/js/dynamic.js
+++ private/js/dynamic.js
@@ -1,67 +1,21 @@
-    var contentEl = document.getElementById('content'),
-        linkEls = document.getElementsByTagName('a'),
-        cats = {
-          fluffy: {
-            content: 'Fluffy!',
-            photo: 'http://placekitten.com/200/200'
-          },
-          socks: {
-            content: 'Socks!',
-            photo: 'http://placekitten.com/280/280'
-          },
-          whiskers: {
-            content: 'Whiskers!',
-            photo: 'http://placekitten.com/350/350'
-          },
-          bob: {
-            content: 'Just Bob.',
-            photo: 'http://placekitten.com/320/270'
-          }
-        };
+var setCurrentPage = function(url) {
+    $('main span').html(url || "/");
+    //$("#menu-nav a[href='" + url + "']").fadeTo(500, 0.3);
+};
 
-    // Switcheroo!
-    function updateContent(data) {
-      if (data == null)
-        return;
-
-      contentEl.textContent = data.content;
-    }
-
-    // Load some mock JSON data into the page
-    function clickHandler(event) {
-      var cat = event.target.getAttribute('href').split('/').pop(),
-          data = cats[cat] || null; // In reality this could be an AJAX request
-
-      updateContent(data);
-
-      // Add an item to the history log
-      history.pushState(data, event.target.textContent, event.target.href);
-
-      return event.preventDefault();
-    }
-
-    // Attach event listeners
-    for (var i = 0, l = linkEls.length; i < l; i++) {
-      linkEls[i].addEventListener('click', clickHandler, true);
-    }
-
-    // Revert to a previously saved state
-    window.addEventListener('popstate', function(event) {
-      console.log('popstate fired!');
-
-      updateContent(event.state);
-    });
-
-$(function () {
-
-$("#info").click(function (e) {
-    $('body').addClass("info-open");
+$('#menu-nav a').click(function(e){
+    e.preventDefault();
+    var targetUrl = $(this).attr('href'),
+        targetTitle = $(this).attr('title');
+    
+    $("#menu-nav a[href='" + window.location.pathname + "']").fadeTo(500, 1.0);
+    
+    window.history.pushState({url: "" + targetUrl + ""}, targetTitle, targetUrl);
+    setCurrentPage(targetUrl);
 });
 
-$(window).click(function(event) {
-	var target = $( event.target );
-	if ( target.is( "#closeModal" ) ) {
-		$('body').removeClass('info-open')
-	}
-});
-});
+window.onpopstate = function(e) {
+    $("#menu-nav a").fadeTo('fast', 1.0);
+    setCurrentPage(e.state ? e.state.url : null);
+};
+
